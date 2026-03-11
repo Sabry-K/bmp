31bbe8f279ae38bab7b30ecd4351ae378301147f
Sabry-K <khaled.sabry@vodafone.com>
2026-03-11 16:47:05 +0200

    Update PreVettedProductController.java
    added a log line

--- catalogComponent/src/main/java/com/vcs/bmp/microservices/catalog/endpoint/PreVettedProductController.java
+++ catalogComponent/src/main/java/com/vcs/bmp/microservices/catalog/endpoint/PreVettedProductController.java
@@ -3,6 +3,8 @@ package com.vcs.bmp.microservices.catalog.endpoint;
 import com.vcs.bmp.microservices.catalog.domain.PreVettedProduct;
 import com.vcs.bmp.microservices.catalog.service.PreVettedProductService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PreVettedProductController {
 
 
+    private static final Logger log = LogManager.getLogger(PreVettedProductController.class);
     private final PreVettedProductService preVettedProductService;
 
     @GetMapping("/pre-vetted")
     public ResponseEntity<Page<PreVettedProduct>> findPreVettedProduct(Pageable pageable) {
-
+        log.info("PRE VETTED END POINT WAS CALLED ");
         return ResponseEntity.ok(this.preVettedProductService.findPreVettedProducts(pageable));
 
     }
