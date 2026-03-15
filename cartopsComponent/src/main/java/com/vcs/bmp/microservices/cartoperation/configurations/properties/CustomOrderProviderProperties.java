f08d3bc9ae4b3384fe63f8eeaf6ee9a97a5b1d4a
Sabry-K <khaled.sabry@vodafone.com>
2026-03-15 11:47:46 +0200

    fixed issue required CustomOrderProviderProperties bean

--- cartopsComponent/src/main/java/com/vcs/bmp/microservices/cartoperation/configurations/properties/CustomOrderProviderProperties.java
+++ cartopsComponent/src/main/java/com/vcs/bmp/microservices/cartoperation/configurations/properties/CustomOrderProviderProperties.java
@@ -3,9 +3,11 @@ package com.vcs.bmp.microservices.cartoperation.configurations.properties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
+@Configuration
 @ConfigurationProperties("broadleaf.cartoperation.orderprovider")
 public class CustomOrderProviderProperties {
     private String url;
