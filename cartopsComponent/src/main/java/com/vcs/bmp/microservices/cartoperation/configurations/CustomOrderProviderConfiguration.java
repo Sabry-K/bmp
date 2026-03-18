0df2e5f50cc58796ccf1c077ed1f052faa43527c
Sabry-K <khaled.sabry@vodafone.com>
2026-03-18 07:27:30 +0200

    added ssl verification disabling logic

--- cartopsComponent/src/main/java/com/vcs/bmp/microservices/cartoperation/configurations/CustomOrderProviderConfiguration.java
+++ cartopsComponent/src/main/java/com/vcs/bmp/microservices/cartoperation/configurations/CustomOrderProviderConfiguration.java
@@ -6,6 +6,8 @@ import com.broadleafcommerce.common.extension.TypeFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vcs.bmp.microservices.cartoperation.configurations.properties.CustomOrderProviderProperties;
 import com.vcs.bmp.microservices.cartoperation.service.provider.CustomOrderProvider;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -17,10 +19,16 @@ import org.springframework.security.oauth2.client.web.reactive.function.client.S
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import reactor.netty.http.client.HttpClient;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 
 import javax.net.ssl.SSLException;
 import java.util.function.Supplier;
 
+@Slf4j
 @Configuration
 @EnableConfigurationProperties(SSLVerificationProperties.class)
 public class CustomOrderProviderConfiguration {
@@ -32,22 +40,37 @@ public class CustomOrderProviderConfiguration {
             ObjectMapper objectMapper,
             SSLVerificationProperties sslVerificationProperties) throws SSLException {
 
+        // Build Netty HttpClient with SSL disabled conditionally
+        HttpClient httpClient = HttpClient.create();
+
+       log.info("SSL verification is : {} " ,  sslVerificationProperties.isDisabled());
+
+        if (sslVerificationProperties.isDisabled()) {
+            log.warn("------------------ SSl verification is disabled --------------------");
+            SslContext sslContext = SslContextBuilder
+                    .forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE) // ← skips cert check
+                    .build();
+            httpClient = httpClient.secure(sslSpec -> sslSpec.sslContext(sslContext));
+        }
+
+        ReactorClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+
         ExchangeStrategies strategies = ExchangeStrategies
                 .builder()
-                .codecs(clientDefaultCodecsConfigurer -> {
-                    clientDefaultCodecsConfigurer.defaultCodecs().jackson2JsonEncoder(
+                .codecs(configurer -> {
+                    configurer.defaultCodecs().jackson2JsonEncoder(
                             new Jackson2JsonEncoder(objectMapper, MediaType.APPLICATION_JSON));
-                    clientDefaultCodecsConfigurer.defaultCodecs().jackson2JsonDecoder(
+                    configurer.defaultCodecs().jackson2JsonDecoder(
                             new Jackson2JsonDecoder(objectMapper, MediaType.APPLICATION_JSON));
-                    clientDefaultCodecsConfigurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024);
+                    configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024);
                 }).build();
-
-        WebClient.Builder webClientBuilder = WebClient.builder();
 
         DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory();
         uriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
 
-        return webClientBuilder
+        return WebClient.builder()
+                .clientConnector(connector)              // ← wire in Netty client
                 .uriBuilderFactory(uriBuilderFactory)
                 .exchangeStrategies(strategies)
                 .apply(oauth2FilterSupplier.get().oauth2Configuration())
