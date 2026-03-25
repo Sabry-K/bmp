a7dceb8745b2a859e589ea5c87f4d0e66750a90f
Sabry-K <khaled.sabry@vodafone.com>
2026-03-25 22:45:26 +0200

    added data route

--- orderComponent/src/main/java/com/vcs/bmp/microservices/order/controller/CustomOrderEndpoint.java
+++ orderComponent/src/main/java/com/vcs/bmp/microservices/order/controller/CustomOrderEndpoint.java
@@ -1,9 +1,14 @@
 package com.vcs.bmp.microservices.order.controller;
 
+import com.broadleafcommerce.common.extension.data.DataRouteByExample;
+import com.broadleafcommerce.common.extension.data.DataRouteByKey;
 import com.broadleafcommerce.data.tracking.core.context.ContextInfo;
+import com.broadleafcommerce.order.provider.RouteConstants;
+import com.broadleafcommerce.order.provider.jpa.domain.JpaOrder;
 import com.vcs.bmp.microservices.order.domain.dto.ReadCustomerOrdersCountWithSpecifiedItemRequest;
 import com.vcs.bmp.microservices.order.service.CustomOrderService;
 import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.N;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
+@DataRouteByKey(RouteConstants.Persistence.ORDER_ROUTE_KEY)
 public class CustomOrderEndpoint {
 
     private final CustomOrderService customOrderService;
